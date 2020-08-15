0f67a6f1d982b01d7161a6bb8b8d7ca039d37a44
subhamsingh-1209 <69725389+subhamsingh-1209@users.noreply.github.com>
2020-08-15 21:04:18 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,30 @@
+window.onload = function myFunctiona() {
+    document.getElementById('card1').style.display = 'block';
+    document.getElementById('card2').style.display = 'none';
+    document.getElementById('card3').style.display = 'none';
+    document.getElementById('card4').style.display = 'none';
+}
+function myFunctiona() {
+    document.getElementById('card1').style.display = 'block';
+    document.getElementById('card2').style.display = 'none';
+    document.getElementById('card3').style.display = 'none';
+    document.getElementById('card4').style.display = 'none';
+}
+function myFunctionb() {
+    document.getElementById('card2').style.display = 'block';
+    document.getElementById('card1').style.display = 'none';
+    document.getElementById('card3').style.display = 'none';
+    document.getElementById('card4').style.display = 'none';
+}
+function myFunctionc() {
+    document.getElementById('card3').style.display = 'block';
+    document.getElementById('card1').style.display = 'none';
+    document.getElementById('card2').style.display = 'none';
+    document.getElementById('card4').style.display = 'none';
+}
+function myFunctiond() {
+    document.getElementById('card4').style.display = 'block';
+    document.getElementById('card1').style.display = 'none';
+    document.getElementById('card2').style.display = 'none';
+    document.getElementById('card3').style.display = 'none';
+}
